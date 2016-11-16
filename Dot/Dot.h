478d93b681b3692fb01e249d1785d597c37ecabc
christian otkjær <christian.otkjaer@gmail.com>
2016-11-16 15:45:35 +0100

    Initial Commit

---
+++ Dot/Dot.h
@@ -0,0 +1,19 @@
+//
+//  Dot.h
+//  Dot
+//
+//  Created by Christian Otkjær on 16/11/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dot.
+FOUNDATION_EXPORT double DotVersionNumber;
+
+//! Project version string for Dot.
+FOUNDATION_EXPORT const unsigned char DotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dot/PublicHeader.h>
+
+
